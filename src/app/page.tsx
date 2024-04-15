04bf54adcdbf60d7f4a5b7e200ee212ab519bedf
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-04-15 18:12:27 +0530

    Navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-    <h1 className="text-4xl text-center mt-4">Hello ....Kiun hila dala naaaa</h1>
+    <h1 className="text-2xl text-center mt-4">Hello ....Kiun hila dala naaaa</h1>
   );
 }
