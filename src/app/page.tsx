9247b72b2cd92cdf834f27a7ec451d1477300a24
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-04-16 16:45:21 +0530

    Hero section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Herosection from "@/components/Herosection";
 
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center mt-4">Hello ....Kiun hila dala naaaa</h1>
+    <>
+    <Herosection/>
+    </>
   );
 }
