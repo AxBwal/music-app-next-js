7367d431fa4a5836b42df88563e2bd6f088b5167
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-08-02 22:08:34 +0530

    Whychooseus updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HerSection from "@/components/HerSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return <>
   <HerSection/>
   <FeaturedCourses/>
+  <WhyChooseUs/>
   </>;
 }
