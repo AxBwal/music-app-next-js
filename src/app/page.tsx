46803d96260b63bb1be3bcf886d56496d418fa80
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-08-02 18:34:38 +0530

    Featured Courses Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HerSection from "@/components/HerSection";
 
 export default function Home() {
   return <>
   <HerSection/>
+  <FeaturedCourses/>
   </>;
 }
